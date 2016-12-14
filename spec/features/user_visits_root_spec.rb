8ffcb4f2645016cf94aee0775586380262d022eb
Noah Berman <noah@noahberman.org>
2016-12-14 09:39:50 -0700

    broken test set as pending for when feature exists

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -7,5 +7,16 @@ describe "user visits the page" do
 
       expect(page).to have_field('navbar-search-field') 
     end
+    
+    xscenario "they enter a zip code in the form" do
+      visit '/'
+      
+      within("#navbar-search-field") do
+        fill_in 'zipcode', with: '80203'
+      end
+      
+      click_button 'Locate'
+      expect(page).to have_content 'Stations'
+    end
   end
 end
