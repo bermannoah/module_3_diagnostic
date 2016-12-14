e29b64f0256b7834d1040c1fc411e014435aef4d
Noah Berman <noah@noahberman.org>
2016-12-14 09:31:10 -0700

    first test is now passing

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -4,8 +4,8 @@ describe "user visits the page" do
   context "a user visits root" do
     scenario "they see a search form" do
       visit '/'
-      
-      expect(page).to have_css('navbar-search') 
+
+      expect(page).to have_field('navbar-search-field') 
     end
   end
 end
